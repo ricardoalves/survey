eedd1a320810d0e46b00398ff884dbfd37baadd3
Ricardo Jorge da Silva Alves <ricardojorge@elementaryOS.(none)>
2013-02-04 16:58:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyorCustomize::Application.config.secret_token = '5493abb674d307612cb548a0d0a4d386dcb3a460c84ca478ba6c2d733af0a145c8eb9b587f29a8c3495509d4d61091d2c180938444fb0531b3f0e0cb344d51b6'
